9be84c4c981f060ac93ae886729141ad9a84fc02
Eric Jiang <eric.jiang@monash.edu>
2017-06-18 16:26:23 +1000

    Fixed Linitng errors using StandardJS

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const microApi = require('micro-api')
-const fs = require('fs-promise')
-var fileS = require('fs');
-
-const handleNone = (() => 'You must use a valid request')
+const handleNone = () => ('You must use a valid request')
 
 const handlePerson = async ({ params: { name }, res }) => {
   try {
-    if(name.toLowerCase() === "eric jiang" ){
-      return name + " is not an asshole"
-    } else if(name.toLowerCase() === "josh parnham"){
+    if (name.toLowerCase() === 'eric jiang') {
+      return name + ' is not an asshole'
+    } else if (name.toLowerCase() === 'josh parnham') {
       return "I don't know what to say"
-    } else if(name.toLowerCase() === "erfan norozi"){
-      return name + " loves Java"
-    } else {  
-      return name + " is an asshole."
+    } else if (name.toLowerCase() === 'erfan norozi') {
+      return name + ' loves Java. XD'
+    } else {
+      return name + ' is an asshole.'
     }
   } catch (err) {
     // Autos to 404
